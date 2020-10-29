8fc81f6a09fab5e40d1b57498261b4ab09797714
kylehao <kylehao@gmail.com>
2020-10-29 23:19:41 +0800

    Add files via upload

---
+++ china111.js
@@ -0,0 +1,9 @@
+mkList("http://index.asia.ga/?/music/cyy/Chunyy-爱在四月雪.mp3","Chunyy-爱在四月雪");
+mkList("http://index.asia.ga/?/music/cyy/Chunyy-安魂曲.mp3","Chunyy-安魂曲");
+mkList("http://index.asia.ga/?/music/cyy/Chunyy-安妮的歌.mp3","Chunyy-安妮的歌");
+mkList("http://index.asia.ga/?/music/cyy/Chunyy-宝贝珍之歌.mp3","Chunyy-宝贝珍之歌");
+mkList("http://index.asia.ga/?/music/cyy/Chunyy-沉睡的森林.mp3","Chunyy-沉睡的森林");
+mkList("http://index.asia.ga/?/music/cyy/Chunyy-沉思曲.wma","Chunyy-沉思曲");
+mkList("http://index.asia.ga/?/music/cyy/Chunyy-初恋的地方.mp3","Chunyy-初恋的地方");
+mkList("http://index.asia.ga/?/music/cyy/Chunyy-春之声圆舞曲 古典钢琴.mp3","Chunyy-春之声圆舞曲 古典钢琴");
+mkList("http://index.asia.ga/?/music/cyy/Chunyy-大溪地清流 米奴哀舞.wma","Chunyy-大溪地清流 米奴哀舞");
